88efc16b3b696b27c3e7a54918200eced6aa1aa5
sanskritisaxena <pavitrasaxena@gmail.com>
2025-09-24 11:24:51 +0530

    using reusble classed form aaa jar and cleaned up code

--- src/main/java/com/opsara/sodagent/config/SecurityConfig.java
+++ src/main/java/com/opsara/sodagent/config/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.opsara.sodagent.config;
 
 import com.opsara.aaaservice.config.AbstractSecurityConfig;
 import org.springframework.context.annotation.Configuration;
-import static com.opsara.aaaservice.constants.Constants.AUTH_ENDPOINTS;
-import static com.opsara.aaaservice.constants.Constants.PUBLIC_ENDPOINTS;
+import static com.opsara.sodagent.constants.Constants.AUTH_ENDPOINTS;
+import static com.opsara.sodagent.constants.Constants.PUBLIC_ENDPOINTS;
 
 @Configuration
 public class SecurityConfig extends AbstractSecurityConfig {
